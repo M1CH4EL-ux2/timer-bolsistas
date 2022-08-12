b80003e34051bbc2cbbf1094fa57cffdfab0c38d
João Michael Lima da Silva <lima.michael@academico.ifrn.edu.br>
2022-08-12 11:43:27 -0300

    Horario atual criado, estilização iniciada do dashboard, falta cronometro do horário

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,26 @@
 import '../css/app.css'
+
+// Hora atual
+const currentTime = () => {
+  const el = document.querySelector('.hour')
+
+  let date = new Date()
+
+  let hh = date.getHours()
+  let mm = date.getMinutes()
+  let ss = date.getSeconds()
+
+  hh = hh < 10 ? `0${hh}` : hh
+  mm = mm < 10 ? `0${mm}` : mm
+  ss = ss < 10 ? `0${ss}` : ss
+
+  let time = `${hh}:${mm}:${ss}`
+  el.innerText = time
+}
+
+currentTime()
+setInterval(currentTime, 1000)
+
+// Horario de entrada e saida
+
+const timer = () => {}
